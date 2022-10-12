8de4f0e77f40260ecead6ab44a37597e339162fb
SophKovalenko <sophiakovalenko@hotmail.com>
2022-10-12 11:22:38 -0400

    Created UI for login system

--- Assets/Scripts/NetworkedClient.cs
+++ Assets/Scripts/NetworkedClient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class NetworkedClient : MonoBehaviour
 {
@@ -79,7 +80,7 @@ public class NetworkedClient : MonoBehaviour
             hostID = NetworkTransport.AddHost(topology, 0);
             Debug.Log("Socket open.  Host ID = " + hostID);
 
-            connectionID = NetworkTransport.Connect(hostID, "192.168.0.107", socketPort, 0, out error); // server is local on network
+            connectionID = NetworkTransport.Connect(hostID, "10.0.235.161", socketPort, 0, out error); // server is local on network
 
             if (error == 0)
             {
